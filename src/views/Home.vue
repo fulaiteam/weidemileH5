920b9008edb1bd156b78719ffb212662bd009072
wenmengqi <574986464@qq.com>
2021-03-26 09:56:48 +0800

    页面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,484 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page_index">
+    <div class="w">
+      <div class="content">
+
+        <div class="slideshow">
+          <van-swipe
+            class="my-swipe"
+            :autoplay="5000"
+            indicator-color="white"
+            :show-indicators=false
+          >
+            <van-swipe-item v-for="(item, index) in 4" :key="index">
+              <img src="@/assets/images/16x9-2021-38ok原文件@2x.png" alt="" class="swipeImg">
+            </van-swipe-item>
+          </van-swipe>
+        </div>
+
+        <div class="new_dynamic">
+          <div class="module_top">
+            <div class="module_top_title">
+              <div class="module_top_title_icon"></div>
+              <div class="module_top_title_font">最新动态</div>
+            </div>
+            <div class="module_top_more">查看更多>></div>
+          </div>
+          <div class="dynamic_content">
+            <div
+              class="dynamic_content_list"
+              v-for="(item, index) in 3"
+              :key="index"
+            >
+              <img src="@/assets/images/uid5270b1616056073914@2x.png" alt="">
+              <div class="dynamic_content_list_right">
+                <div class="dynamic_content_list_right_title">精诚合作，魏德米勒</div>
+                <div class="dynamic_content_list_right_particulars"><div>化繁为简！魏德米</div></div>
+              </div>
+              <div class="dynamic_content_list_to">
+                <img src="@/assets/images/返回 拷贝 3@2x.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="trade_show">
+          <div class="module_top">
+            <div class="module_top_title">
+              <div class="show_top_title_icon"></div>
+              <div class="module_top_title_font">展会</div>
+            </div>
+            <div class="module_top_more">查看更多>></div>
+          </div>
+          <div class="show_content">
+            <div class="show_content_tab">
+              <van-tabs
+                v-model="active"
+                title-active-color='#EB8C00'
+                color='#EB8C00'
+              >
+                <van-tab title="线上HIMI" :title-class="{tabClass: active==0}"></van-tab>
+                <van-tab title="慕尼黑电子展" :title-class="{tabClass: active==1}"></van-tab>
+                <van-tab title="成都自动化展" :title-class="{tabClass: active==2}"></van-tab>
+              </van-tabs>
+            </div>
+            <div class="show_content_list">
+              <div class="show_content_list_box" :class="{show_content_list_handle_box: active==0}">
+                <img src="@/assets/images/automation@2x.png" alt="">
+              </div>
+              <div class="show_content_list_box" :class="{show_content_list_handle_box: active==1}">
+                <img src="@/assets/images/automation@2x.png" alt="">
+              </div>
+              <div class="show_content_list_box" :class="{show_content_list_handle_box: active==2}">
+                <img src="@/assets/images/automation@2x.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="online_meeting">
+          <div class="module_top">
+            <div class="module_top_title">
+              <div class="meeting_top_title_icon"></div>
+              <div class="module_top_title_font">在线研讨会</div>
+            </div>
+          </div>
+          <div class="meeting_content">
+            <div
+              class="meeting_content_list"
+              v-for="(item, index) in 5"
+              :key="index"
+            >
+              <img src="@/assets/images/u=3281973023,1261252532&fm=26&gp=0@2x.png" alt="">
+              <div class="meeting_content_list_right">
+                <div class="meeting_content_list_right_title">在线研讨会 各路大神教你怎么做</div>
+                <div class="meeting_content_list_right_info">
+                  <div class="meeting_content_list_right_info_left">
+                    <div class="meeting_content_list_right_info_left_backer">主持人：阿呆</div>
+                    <div class="meeting_content_list_right_info_left_time">时间：2021-03-04</div>
+                  </div>
+                  <div class="meeting_content_list_right_info_right">
+                    <span>查看</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="online_class">
+          <div class="module_top">
+            <div class="module_top_title">
+              <div class="class_top_title_icon"></div>
+              <div class="module_top_title_font">在线大课堂</div>
+            </div>
+            <div class="module_top_more">查看更多>></div>
+          </div>
+          <div class="class_content">
+            <div
+              class="class_content_list"
+              v-for="(item, index) in 3"
+              :key="index"
+            >
+              <img src="@/assets/images/uid5270b1592184016427@2x.png" alt="">
+              <div class="class_content_list_right">
+                <div class="class_content_list_right_left">
+                  <div class="class_content_list_right_title">魏德米勒OMNIMATE®总线连接解决方案</div>
+                  <div class="class_content_list_right_particulars"><div>模块化和定制的解决方案</div></div>
+                </div>
+                <div class="class_content_list_right_right">
+                  <img src="@/assets/images/返回 拷贝 3@2x.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      active: 1,  /* tabbar切换栏下标 */
+      tabbarTitle: ["线上HIMI","慕尼黑电子展","成都自动化展"]
+    };
+  },
+  watch: {
+    active(a, b) {
+      console.log(a, b)
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+
+  .page_index {
+    background: #EEEEEE;
+  }
+  .w {
+    padding: 27/540 * 100vw 30/540 * 100vw;
+  }
+
+
+  .slideshow {
+    height: 265/540 * 100vw;
+    margin-bottom: 45/540 * 100vw;
+    border-radius: 10/540 * 100vw;
+    overflow: hidden;
+  }
+  .my-swipe {
+    width: 100%;
+    height: 100%;
+  }
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+  .swipeImg {
+    width: 100%;
+    height: 100%;
+  }
+
+
+  .new_dynamic {
+    margin-bottom: 23/540 * 100vw;
+  }
+  .module_top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 22.5/540 * 100vw;
+  }
+  .module_top_title {
+    display: flex;
+    align-items: center;
+  }
+  .module_top_title_icon {
+    width: 20.5/540 * 100vw;
+    height: 20.5/540 * 100vw;
+    background: url('../assets/images/动态@2x.png') no-repeat;
+    background-size: 20.5/540 * 100vw;
+    margin-right: 12/540 * 100vw;
+  }
+  .module_top_title_font {
+    font-size: 18/540 * 100vw;
+    font-family: Source Han Sans CN;
+    font-weight: bold;
+    color: #EB8C00;
+    letter-spacing: 1/540 * 100vw
+  }
+  .module_top_more {
+    font-size: 15/540 * 100vw;
+    font-family: Source Han Sans CN;
+    font-weight: 400;
+    color: #8E8E8E;
+  }
+  .dynamic_content {
+    border-radius: 10/540 * 100vw;
+    overflow: hidden;
+  }
+  .dynamic_content_list {
+    display: flex;
+    position: relative;
+    background: #fff;
+    padding: 22/540 * 100vw;
+    margin-bottom: 1px;
+  }
+  .dynamic_content_list img {
+    width: 122/540 * 100vw;
+    height: 95/540 * 100vw;
+  }
+  .dynamic_content_list_right {
+    margin-left: 23/540 * 100vw;
+  }
+  .dynamic_content_list_right_title {
+      width: 197/540 * 100vw;
+      font-size: 18/540 * 100vw;
+      font-family: Source Han Sans CN;
+      font-weight: 400;
+      color: #EB8C00;
+      line-height: 21/540 * 100vw;
+      text-align: justify;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      word-break: break-all;
+  }
+  .dynamic_content_list_right_particulars {
+      font-size: 15/540 * 100vw;
+      color: #8E8E8E;
+      line-height: 19/540 * 100vw;
+      text-align: justify; /* 末尾对齐 */
+      padding-top: 10/540 * 100vw;
+      margin-top: 11/540 * 100vw;
+      border-top: 1/540 * 100vw dashed #EB8C00;
+  }
+  .dynamic_content_list_right_particulars div{
+    width: 300.5/540 * 100vw;
+    overflow: hidden;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+    -webkit-transform:scale(0.8);
+    margin-left: -30/540 * 100vw;
+  }
+  .dynamic_content_list_to {
+    position: absolute;
+    right: 13.5/540 * 100vw;
+    bottom: 13.5/540 * 100vw;
+    width: 23.5/540 * 100vw;
+    height: 24.5/540 * 100vw;
+  }
+  .dynamic_content_list_to img {
+    width: 100%;
+    height: 100%;
+  }
+
+
+  .trade_show {
+    margin-bottom: 23/540 * 100vw;
+  }
+  .show_top_title_icon {
+    width: 20.5/540 * 100vw;
+    height: 20.5/540 * 100vw;
+    background: url('../assets/images/在线展会@2x.png') no-repeat;
+    background-size: 20.5/540 * 100vw;
+    margin-right: 12/540 * 100vw;
+  }
+  .show_content {
+    background: #fff;
+    border-radius: 10/540 * 100vw;
+    overflow: hidden;
+  }
+  .show_content_tab {
+    padding-top: 18/540 * 100vw;
+  }
+  .show_content_list {
+    height: 216.5/540 * 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 37/540 * 100vw 13/540 * 100vw 41/540 * 100vw;
+  }
+  .show_content_list_box {
+    width: 128.5/540 * 100vw;
+    height: 170/540 * 100vw;
+  }
+  .show_content_list_box img {
+    width: 100%;
+    height: 100%;
+  }
+  .show_content_list_handle_box {
+    width: 164/540 * 100vw;
+    height: 216.5/540 * 100vw;
+  }
+  .show_content_list_handle_box img {
+    width: 100%;
+    height: 100%;
+  }
+  .van-tabs--line /deep/ .van-tabs__wrap {
+    height: 28.5/540 * 100vw;
+  }
+  /deep/ .van-tab {
+    font-size: 15/540 * 100vw;
+  }
+  /deep/ .van-tabs__line {
+    width: 60/540 * 100vw;
+    height: 1.5/540 * 100vw;
+  }
+  /deep/ .tabClass {
+    font-size: 20/540 * 100vw;
+  }
+
+
+  .online_meeting {
+    margin-bottom: 23/540 * 100vw;
+  }
+  .meeting_top_title_icon {
+    width: 20.5/540 * 100vw;
+    height: 20.5/540 * 100vw;
+    background: url('../assets/images/课堂@2x.png') no-repeat;
+    background-size: 20.5/540 * 100vw;
+    margin-right: 12/540 * 100vw;
+  }
+  .meeting_content_list {
+    display: flex;
+    align-items: center;
+    background: #fff;
+    margin-bottom: 25.5/540 * 100vw;
+    border-radius: 5/540 * 100vw;
+  }
+  .meeting_content_list img {
+    width: 211.5/540 * 100vw;
+    height: 152.5/540 * 100vw;
+  }
+  .meeting_content_list_right {
+    padding: 15/540 * 100vw 21/540 * 100vw 15/540 * 100vw 15.5/540 * 100vw;
+  }
+  .meeting_content_list_right_title {
+    height: 56/540 * 100vw;
+    font-size: 18/540 * 100vw;
+    color: #EB8C00;
+    line-height: 30/540 * 100vw;
+    margin-bottom: 18/540 * 100vw;
+    text-align: justify;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+  }
+  .meeting_content_list_right_info {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+  .meeting_content_list_right_info_left {
+    font-size: 14/540 * 100vw;
+    color: #8E8E8E;
+  }
+  .meeting_content_list_right_info_left_backer {
+    -webkit-transform:scale(0.8);
+    margin-left: -14/540 * 100vw;
+  }
+  .meeting_content_list_right_info_left_time {
+    -webkit-transform:scale(0.8);
+    margin-left: -14/540 * 100vw;
+  }
+  .meeting_content_list_right_info_right {
+    width: 57.3/540 * 100vw;
+    height: 28.65/540 * 100vw;
+    line-height: 28.65/540 * 100vw;
+    text-align: center;
+    border-radius: 10/540 * 100vw;
+    background: #EB8C00;
+    color: #fff;
+    font-size: 15/540 * 100vw;
+  }
+  .meeting_content_list_right_info_right span {
+    display: inline-block;
+    -webkit-transform:scale(0.9);
+  }
+
+
+  .class_content {
+    border-radius: 10/540 * 100vw;
+    overflow: hidden;
+  }
+  .class_top_title_icon {
+    width: 20.5/540 * 100vw;
+    height: 20.5/540 * 100vw;
+    background: url('../assets/images/课堂.png') no-repeat;
+    background-size: 20.5/540 * 100vw;
+    margin-right: 12/540 * 100vw;
+  }
+  .class_content_list {
+    display: flex;
+    align-items: center;
+    background: #fff;
+    padding: 22/540 * 100vw;
+    margin-bottom: 1px;
+  }
+  .class_content_list img {
+    width: 115.5/540 * 100vw;
+    height: 116/540 * 100vw;
+    border-radius: 50%;
+  }
+  .class_content_list_right {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 38.5/540 * 100vw;
+  }
+  .class_content_list_right_title {
+    width: 209/540 * 100vw;
+    font-size: 18/540 * 100vw;
+    color: #EB8C00;
+    line-height: 21/540 * 100vw;
+    text-align: justify;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+  }
+  .class_content_list_right_particulars {
+    font-size: 15/540 * 100vw;
+    color: #8E8E8E;
+    line-height: 19/540 * 100vw;
+    text-align: justify; /* 末尾对齐 */
+    margin-top: 11/540 * 100vw;
+  }
+  .class_content_list_right_particulars div{
+    width: 300.5/540 * 100vw;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+    -webkit-transform:scale(0.7);
+    margin-left: -45/540 * 100vw;
+  }
+  .class_content_list_right_right {
+    width: 23.5/540 * 100vw;
+    height: 24.5/540 * 100vw;
+  }
+  .class_content_list_right_right img {
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
